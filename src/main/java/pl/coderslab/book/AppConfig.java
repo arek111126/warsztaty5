aedb2e81313db71289d3f1dc451e0953f1e147d8
Arkadiusz <arek111126@gmail.com>
2019-08-17 13:05:56 +0200

    first

---
+++ src/main/java/pl/coderslab/book/AppConfig.java
@@ -0,0 +1,12 @@
+package pl.coderslab.book;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class AppConfig extends WebMvcConfigurerAdapter {
+}
